b2fc39d4474862475dab870e4877581875a2b395
MichaelRand0 <mihailstepanov.stepanoff2015@yandex.ru>
2023-06-21 22:12:04 +0300

    :green_heart: Animations added

--- src/modules/PostList.vue
+++ src/modules/PostList.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h3 class="mb-5 text-xl font-bold">POSTS</h3>
-    <div v-if="posts.length > 0">
-      <PostItem :key="post.id" @remove="$emit('remove', post)" v-for="post in posts" :post="post" />
-    </div>
+    <transition-group name="list" v-if="posts.length > 0">
+      <PostItem
+        class="list-item"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+        v-for="post in posts"
+        :post="post"
+      />
+    </transition-group>
     <h4 v-else class="text-red-600 font-bold text-lg">Список постов пуст...</h4>
   </div>
 </template>
@@ -19,3 +25,21 @@ export default {
   },
 }
 </script>
+<style>
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+  list-style: none;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.4s;
+}
+.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(120px);
+}
+.list-move {
+  transition: transform 0.4s;
+}
+</style>
